acaf6414a0563d0cb4cf6acc929726ba219a33c9
Daniil Golovats <daniil.golovats@tktk.ee>
2024-04-03 21:49:19 +0300

    add week6 code

---
+++ Arrays/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snacks = { "sushi", "pizza", "burger", "chicken wings", "Asian takeaway" };
+
+Random rand = new Random();
+
+int randomIndex = rnd.next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we are going to eat {snacks[randomIndex]}.");
